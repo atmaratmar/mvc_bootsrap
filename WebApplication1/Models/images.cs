0140a35d5e494a9e98cddf62a42d7dcd312e2b00
atmar kohistany <atmar@hotmail.dk>
2018-02-22 14:55:09 +0100

    g

---
+++ WebApplication1/Models/images.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public partial class images
+    {
+        
+    }
+}
